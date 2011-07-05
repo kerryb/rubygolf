1d1a7203c709c1a36469d26286bc667f142c987f
Kerry Buckley <kerryjbuckley@gmail.com>
2011-07-05 20:21:53 +0100

    Non-recursive factorial

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,4 +1,4 @@
-class Golf;class<<self;def hole1 a;a.inject &:*;end;def hole2 a;a.split.sort{|b,c|b[1]<=>c[1]}.join " ";end;def hole3 a;a==1?1:a*hole3(a-1);end;def hole4 a;a.map{|b|b.sub(/(ma.*)/,'hat(\1)').sub(/(do.*)\)/,'\1(bone))').sub /cat(.*)/,'dead\1'};end
+class Golf;class<<self;def hole1 a;a.inject &:*;end;def hole2 a;a.split.sort{|b,c|b[1]<=>c[1]}.join " ";end;def hole3 a;(2..a).inject &:*;end;def hole4 a;a.map{|b|b.sub(/(ma.*)/,'hat(\1)').sub(/(do.*)\)/,'\1(bone))').sub /cat(.*)/,'dead\1'};end
 def hole5
 end
 end;end
