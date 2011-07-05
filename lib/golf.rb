bb21cc24c3612ad8857ae65305f30fe84efc15e4
Kerry Buckley <kerryjbuckley@gmail.com>
2011-07-05 19:57:08 +0100

    Hole 5

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,5 @@
-class Golf;class<<self;def hole1 a;a.inject{|a,b|a*b};end;def hole2 a;a.split.sort{|a,b| a[1]<=>b[1]}.join " ";end;def hole3 a;a==1?1:a*hole3(a-1);end
+class Golf;class<<self;def hole1 a;a.inject{|b,c|b*c};end;def hole2 a;a.split.sort{|b,c| b[1]<=>c[1]}.join " ";end;def hole3 a;a==1?1:a*hole3(a-1);end
+def hole4 a
+  a.map{|b|b.sub(/(man.*)/,'hat(\1)').sub(/(dog.*)\)/,'\1(bone))').sub /cat(.*)/,'dead\1'}
+end
 end;end
