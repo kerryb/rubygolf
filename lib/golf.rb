34eff1e9e30d206f2d867ba384f718b74da9c956
Kerry Buckley <kerryjbuckley@gmail.com>
2011-07-05 21:00:32 +0100

    More tiny optimisations

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,3 +1,2 @@
-class Golf;class<<self;def hole1 a;a.inject &:*;end;def hole2 a;a.split.sort{|b,c|b[1]<=>c[1]}.join" ";end;def hole3 a;(2..a).inject &:*;end;def hole4 a;a.map{|b|b.sub(/(ma.*)/,'hat(\1)').sub(/(do.*)\)/,'\1(bone))').sub /cat(.*)/,'dead\1'};end
-def hole5 a;(1..a.length).map{|b|a.each_cons(b).to_a}.uniq.flatten(1);end;def hole6 a;(1..a).map{|b|c=(b%3==0?"fizz":"")+(b%5==0?"buzz":"");c.empty? ? b: c};end
+class Golf;class<<self;def hole1 a;a.inject &:*;end;def hole2 a;a.split.sort{|b,c|b[1]<=>c[1]}.join" ";end;def hole3 a;(2..a).inject &:*;end;def hole4 a;a.map{|b|b.sub(/(ma.*)/,'hat(\1)').sub(/(do.*)\)/,'\1(bone))').sub /cat(.*)/,'dead\1'};end;def hole5 a;(1..a.size).map{|b|a.each_cons(b).to_a}.uniq.flatten(1);end;def hole6 a;(1..a).map{|b|c=(b%3==0?"fizz":"")+(b%5==0?"buzz":"");c.empty? ? b: c};end
 end;end
