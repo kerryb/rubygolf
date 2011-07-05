f045a6de9fb2b500490a518c357ae5a527e3aa74
Kerry Buckley <kerryjbuckley@gmail.com>
2011-07-05 19:46:36 +0100

    Compress working holes onto one line

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,7 +1,2 @@
-class Golf
-  class<<self
-    def hole1 a;a.inject{|a,b|a*b};end
-    def hole2 a;a.split.sort{|a,b| a[1]<=>b[1]}.join " ";end
-    def hole3 a;a==1?1:a*hole3(a-1);end
-  end
-end
+class Golf;class<<self;def hole1 a;a.inject{|a,b|a*b};end;def hole2 a;a.split.sort{|a,b| a[1]<=>b[1]}.join " ";end;def hole3 a;a==1?1:a*hole3(a-1);end
+end;end
